0515628817f1009279b553d16eeb1e3edb35ba8a
Pranaya Geetha <96567109+Pranayageetha@users.noreply.github.com>
2022-11-30 19:49:12 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ProfileScreen from './ProfileScreen';
-import TabNav from "./TabNav";
-import EditorScreen from './WriteInterviewExp';
-export default function App() {
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './Home';
+import SignUp from './SignUp';
+import Login from './Login';
+import TabNav from './TabNav';
+
+  const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-<ProfileScreen />
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Home} /> 
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="TabNav" component={TabNav}/>
+      </Stack.Navigator>
+      
+    </NavigationContainer>
   );
 }
 
+export default App;
